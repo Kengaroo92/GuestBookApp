546ebf2a9d2abdf61c8ef4090a406d25e05c55e0
Kenan Bjelosevic <kbjelose1992@gmail.com>
2022-07-28 20:27:20 -0600

    Changing approach.

---
+++ GuestBook/GuestLogic.cs
@@ -0,0 +1,44 @@
+﻿namespace GuestBook
+{
+    public static class GuestLogic
+    {
+        public static void WelcomeMessage()
+        {
+            //WelcomeMessage
+            Console.WriteLine("**********************");
+            Console.WriteLine("Welcome to the Party!!");
+            Console.WriteLine("**********************");
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************");
+            Console.WriteLine("We like to keep track of all of our guests in this application.");
+            Console.WriteLine("***************************************************************");
+            Console.WriteLine();
+        }
+
+        public static string GetPartyName()
+        {
+            //GetPartyName
+            Console.Write("Enter your party name: ");
+            string output = Console.ReadLine();
+
+            return output;
+        }
+
+        public static int GetPartySize()
+        {
+            bool isValidNumber;
+            int output;
+
+            do
+            {
+                //GetPartySize
+                Console.Write("How many people are in your party: ");
+                string partySizeText = Console.ReadLine();
+                isValidNumber = int.TryParse(partySizeText, out output);
+            } while (isValidNumber == false);
+
+            return output;
+        }
+    }
+}
--- GuestBook/Program.cs
+++ GuestBook/Program.cs
@@ -2,57 +2,17 @@
 
 //Keep in mind DRY. Don't Repeat Yourself - Use as many methods as you can to avoid repeat code.
 
-Console.WriteLine("Welcome to the Party!!");
-Console.WriteLine("We like to keep track of all of our guests in this application.");
-Console.WriteLine("Before you come in and have a good time, can you tell me your name?");
-Console.Write("Enter your name: ");
+int totalGuests = 0;
+string continueLooping;
 
-GuestBookMethods.GuestNames();
+GuestLogic.WelcomeMessage();
 
-List<int> totalGuestCount = new List<int>();
-
-int guestCount = GuestBookMethods.GuestNames().Count;
-
-totalGuestCount.Add(guestCount);
-
-GuestBookMethods.MoreGuests();
-
-while (GuestBookMethods.MoreGuests() == "yes")
+do
 {
-    Console.WriteLine("Welcome new guest. What is your name?");
-    Console.Write("Enter your name: ");
-    GuestBookMethods.GuestNames();
-}
+    string partyName = GuestLogic.GetPartyName();
 
-for (int i = 0; i < guestCount; i++)
-{
-    totalGuestCount.Add(i);
-}
+    totalGuests += GuestLogic.GetPartySize();
 
-Console.WriteLine($"That's it. We have {GuestBookMethods.GuestCount().Count} guests at the party.");
-Console.WriteLine("Let the party begin!");
-
-
-
-
-
-
-
-//Welcome the guests to your party. WelcomeToParty()
-
-//GuestBookMethods.WelcomeToParty();
-
-//Ask the guest for their name. - GuestsName()
-
-//GuestBookMethods.GuestNames();
-
-//GuestBookMethods.AreThereMoreGuests();
-
-//Ask the guest how many people are in their family and each of their names. - FamilyMemberNames() , FamilyCount()
-
-//Keep track of how many people are in the guests family. - FamilyCount()
-
-//Keep track of how many people there are total at the entire party. - TotalPersonCount()
-
-//Print out the guest book, including the names of the individual families, the individual members of each family,
-//the total count in each family, and the total count of people at the entire party. 
+    Console.Write("Are there more guests coming (yes/no): ");
+    continueLooping = Console.ReadLine();
+} while (continueLooping.ToLower() == "yes");
